67a5ec7dbdadf41fed41681962da2ca898929ecd
Issam-elka <80962666+Issam-elka@users.noreply.github.com>
2021-05-10 10:54:00 +0200

    DomIntro

---
+++ main.js
@@ -0,0 +1,12 @@
+let H1 = document.getElementsByTagName('h1');
+console.log(H1);
+
+let H3 = document.getElementsByTagName('h3');
+console.log(H3);
+
+let p = document.getElementsByTagName('p');
+console.log(p);
+console.log(p[1]);
+
+let LI = document.getElementsByTagName('li');
+console.log(LI[2]);
